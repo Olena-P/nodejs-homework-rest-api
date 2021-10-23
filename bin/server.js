cdea2aaed61f99e31569f4246687e4065f9e8b92
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2021-10-24 01:20:15 +0300

    fix

--- bin/server.js
+++ bin/server.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const app = require("../app");
 
-const { PORT = 3000 } = process.env;
-const { DB_USER, DB_USER_PASS, DB_NAME } = process.env;
+const { DB_USER, DB_USER_PASS, DB_NAME, PORT = 3000 } = process.env;
 const DB_HOST = `mongodb+srv://${DB_USER}:${DB_USER_PASS}@cluster0.v3vel.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
 mongoose
@@ -11,7 +10,9 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => app.listen(PORT))
+  .then(() => app.listen(PORT, () => {
+    console.log('Database connection successful')
+  }))
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
